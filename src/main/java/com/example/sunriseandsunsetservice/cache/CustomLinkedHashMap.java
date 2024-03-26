ea81910b72cf062655acea8b41908c6b8d270273
allapugacheva <verhovinandrej@gmail.com>
2024-03-26 08:30:56 +0300

    third lab edit6

--- src/main/java/com/example/sunriseandsunsetservice/cache/CustomLinkedHashMap.java
+++ src/main/java/com/example/sunriseandsunsetservice/cache/CustomLinkedHashMap.java
@@ -2,6 +2,7 @@ package com.example.sunriseandsunsetservice.cache;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Custom linked hash map class.
@@ -18,5 +19,24 @@ public class CustomLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
   @Override
   protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
     return size() > maxSize;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof CustomLinkedHashMap<?, ?> that)) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    return Objects.equals(maxSize, that.maxSize);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), maxSize);
   }
 }
--- src/main/java/com/example/sunriseandsunsetservice/cache/InMemoryCache.java
+++ src/main/java/com/example/sunriseandsunsetservice/cache/InMemoryCache.java
@@ -1,6 +1,5 @@
 package com.example.sunriseandsunsetservice.cache;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 import org.springframework.stereotype.Component;
 
